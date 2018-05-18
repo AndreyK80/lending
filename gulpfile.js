6584aad3c1ef59dee8b4b219d92aaefae3a33c51
AndreyK80 <adpt3adpt@gmail.com>
2018-05-18 08:42:30 +0300

    Header was add

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp'),
     pug = require('gulp-pug'),
     spritesmith = require('gulp.spritesmith'),
     rimraf = require('rimraf'),
-    browserSync = require("browser-sync").create();
+    autoprefixer = require("gulp-autoprefixer"),
+    browserSync = require("browser-sync").create(),
+    plumber = require("gulp-plumber");
 
 /*------ server -----*/
 gulp.task("server", function () {
@@ -24,14 +26,20 @@ gulp.watch("dist/**/*").on('change', browserSync.reload);
 /*------ pug -----*/
 gulp.task('html', function buildHTML() {
     return gulp.src('src/template/index.pug')
+        .pipe(plumber())
         .pipe(pug({pretty: true}))
         .pipe(gulp.dest('dist'));
 });
 
 /*------ sass -----*/
-gulp.task('sass', function () {
-    return gulp.src('src/styles/**/main.sass')
+gulp.task('scss', function () {
+    return gulp.src('src/styles/main.scss')
+        .pipe(plumber())
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('dist/css'));
 });
 
@@ -48,32 +56,35 @@ gulp.task('sprite', function (cb) {
     cb();
 });
 
+/*------ copy:fonts -----*/
 gulp.task('copy:fonts', function () {
     return gulp.src('src/fonts/**/*.*')
         .pipe(gulp.dest('dist/fonts'));
 });
 
-
+/*------ copy:img -----*/
 gulp.task('copy:img', function () {
     return gulp.src('src/img/**/*.*')
         .pipe(gulp.dest('dist/img'));
 });
 
-
+/*------ clean -----*/
 gulp.task('clean', function del(cb) {
     return rimraf('dist', cb);
 });
 
+/*------ copy -----*/
 gulp.task('copy', gulp.parallel('copy:fonts', 'copy:img'));
 
+/*------ watch -----*/
 gulp.task('watch', function () {
-   gulp.watch('src/styles/**/*.*', gulp.series('sass'));
-   gulp.watch('src/template/*.*', gulp.series('html'));
+   gulp.watch('src/styles/**/*.*', gulp.series('scss'));
+   gulp.watch('src/template/**/*.*', gulp.series('html'));
 });
 
-
+/*------ default -----*/
 gulp.task('default', gulp.series('clean',
-    gulp.parallel('html', 'sass', 'copy:fonts', 'sprite', 'copy'),
+    gulp.parallel('html', 'scss', 'copy:fonts', 'sprite', 'copy'),
     gulp.parallel('watch', 'server')
     )
 );
